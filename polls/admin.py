22aae2307136f3756f1463734ced518c17df9617
zacktan <zacktanzx1231@gmail.com>
2025-11-27 01:47:44 +0800

    Extended the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,19 @@ from .models import Question, Choice
 # Register your models here.
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+
+# admin.site.register(Question)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra =  3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date","question_text"]
+    list_display = ["question_text", "pub_date", "was_published_recently"] #This changes the list display
+    inlines = [ChoiceInLine] 
+
+admin.site.register(Question,QuestionAdmin)
+
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 import datetime
 # Create your models here.
 
@@ -8,6 +9,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?"
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
