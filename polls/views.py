7bdfab21b9f9c9d72000fe54bcd9947501ed8250
zacktan <zacktanzx1231@gmail.com>
2025-11-26 18:39:20 +0800

    Apply .gitignore properly

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
 from .models import Question
-from django.template import loader
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     output = ", ".join([q.question_text for q in latest_question_list])
+#     context = {"latest_question_list": latest_question_list}
+#     return HttpResponse(template.render(context,request))
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    output = ", ".join([q.question_text for q in latest_question_list])
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context,request))
+    context = {"latest_question_list": latest_question_list}    
+    return render(request,"polls/index.html", context)
 
-def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+# def detail(request, question_id):
+#     try : 
+#         question = Question.objects.get(id=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")     
+#     return render(request, "polls/detail.html", {"question":question})
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = f"You're looking at the results of questions {question_id}"
