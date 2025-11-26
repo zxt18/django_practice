c3e8eb8682e4fb82a70bc05d807e9f6aea09120d
zacktan <zacktanzx1231@gmail.com>
2025-11-26 21:31:23 +0800

    ListView is for a view class for list of objects, DetailView for a specific model's object using primary key

--- polls/views.py
+++ polls/views.py
@@ -1,35 +1,51 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Choice, Question
 from django.db.models import F
-
+from django.views import generic
 # def index(request):
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
 #     template = loader.get_template("polls/index.html")
 #     output = ", ".join([q.question_text for q in latest_question_list])
 #     context = {"latest_question_list": latest_question_list}
 #     return HttpResponse(template.render(context,request))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list" #Sets the context variable name
+    
+    def get_queryset(sefl):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}    
-    return render(request,"polls/index.html", context)
+class DetailView(generic.DetailView):
+    #Each class view needs to know which model it's operating on, either that or define get_queryset()
+    model = Question
+    template_name = "polls/detail.html"
+    
 
-# def detail(request, question_id):
-#     try : 
-#         question = Question.objects.get(id=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")     
-#     return render(request, "polls/detail.html", {"question":question})
-def detail(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name="polls/results.html"    
+        
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}    
+#     return render(request,"polls/index.html", context)
 
-def results(request, question_id):
-    # response = f"You're looking at the results of questions {question_id}"
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question":question})
+# # def detail(request, question_id):
+# #     try : 
+# #         question = Question.objects.get(id=question_id)
+# #     except Question.DoesNotExist:
+# #         raise Http404("Question does not exist")     
+# #     return render(request, "polls/detail.html", {"question":question})
+# def detail(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+# def results(request, question_id):
+#     # response = f"You're looking at the results of questions {question_id}"
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {"question":question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -48,9 +64,7 @@ def vote(request, question_id):
         
     else : 
         selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        
-        
+        selected_choice.save()        
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
     
